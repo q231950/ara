a77ee289ff9b2d2054575e465ccbbea2a64cd2bb
Martin Kim Dung-Pham <kim@elbedev.com>
2016-05-28 13:52:32 +0200

    Parse cmd line argv

--- lib/ara.ex
+++ lib/ara.ex
@@ -1,8 +1,35 @@
 defmodule Ara do
-  require Issues
 
-  def issues do
-    Issues.CLI.main ["erlang", "rebar3", "10"]
+  def main(argv) do
+    argv
+    |> parse_args
+    |> process
+  end
+
+  def parse_args(argv) do
+    parse = OptionParser.parse( argv, switches: [ help: :boolean , pullrequests: :boolean],
+                                       aliases: [ h: :help , pr: :pullrequests])
+
+     case parse do
+        { [ help: true ], _, _ }
+          -> :help
+
+        { [pullrequests: true ], _, _ }
+          -> :pullrequests
+
+          { _, ["pr"], _ }
+            -> :pullrequests
+
+         _ -> :help
+     end
+  end
+
+  defp process( :help ) do
+    IO.puts "Usage: ara [-pr | -h]"
+  end
+
+  defp process( :pullrequests ) do
+    open_pull_requests
   end
 
   def open_pull_requests do
@@ -12,4 +39,5 @@ defmodule Ara do
     |> TableRex.quick_render!(header)
     |> IO.puts
   end
+
 end
--- mix.exs
+++ mix.exs
@@ -7,30 +7,18 @@ defmodule Ara.Mixfile do
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     escript: [main_module: Ara],
      deps: deps]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger, :httpoison, :issues, :table_rex]]
+    [applications: [:logger, :httpoison, :table_rex]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     [
       { :httpoison, "~> 0.8" },
       { :poison, "~> 2.0" },
-      { :issues, path: "../issues" },
       { :table_rex, "~> 0.8.0" }
     ]
   end
