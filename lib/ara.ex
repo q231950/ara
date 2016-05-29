f53e54b1105e1061b76101fc78132be3a516352d
Martin Kim Dung-Pham <kim@elbedev.com>
2016-05-29 20:10:18 +0200

    Differentiate between checking out and quitting

--- lib/ara.ex
+++ lib/ara.ex
@@ -25,27 +25,51 @@ defmodule Ara do
     end
   end
 
-  defp process( :help ) do
+  def process( :help ) do
     IO.puts "Usage: ara -pr <owner> <repository>"
   end
 
-  defp process( { :pr, owner, repository} ) do
+  def process( { :pr, owner, repository} ) do
     fetch_user
     |> open_pull_requests( owner, repository )
+
   end
 
-  def fetch_user do
+  defp fetch_user do
     Ara.GitHubUser.fetch
-    |> IO.inspect
   end
 
-  def open_pull_requests( user, owner, repository ) do
-    header = ["#", "Title", "Author"]
+  defp open_pull_requests( user, owner, repository ) do
     PullRequests.GitHubPullRequests.fetch( owner, repository )
     |> Enum.filter( fn pr -> assignee_login(pr.assignee) == user.login end )
+    |> render_pull_requests
+  end
+
+  defp render_pull_requests(pull_requests) when length(pull_requests) > 0 do
+    header = ["#", "Title", "Author"]
+    pull_requests
     |> Enum.map( fn pr -> [ pr.number, pr.title, pr.user.login ] end )
     |> TableRex.quick_render!(header)
     |> IO.puts
+
+    request_action_for_pull_requests( pull_requests )
+  end
+
+  defp render_pull_requests(_) do
+  end
+
+  defp request_action_for_pull_requests ( pull_requests ) do
+    answer = IO.gets "Checkout a pr? Choose a number to checkout, press q to exit"
+    case answer do
+      "q\n" -> IO.puts ("bye")
+      number -> checkout("repository", "branch")
+    end
+    # Enum.map(pull_requests, fn pr -> pr.number end )
+    # |> IO.inspect
+  end
+
+  defp checkout(repository, branch) do
+    IO.puts "Checkout some branch and repository"
   end
 
   defp assignee_login(assignee) do
